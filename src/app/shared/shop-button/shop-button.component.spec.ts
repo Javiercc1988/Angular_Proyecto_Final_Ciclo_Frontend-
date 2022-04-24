215a3f860619f4431dae6a2e3f5ce5b5ff71332c
Javier Callejón <javicallejon88@gmail.com>
2022-04-24 12:17:59 +0200

    Home page and parallax effects

---
+++ src/app/shared/shop-button/shop-button.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShopButtonComponent } from './shop-button.component';
+
+describe('ShopButtonComponent', () => {
+  let component: ShopButtonComponent;
+  let fixture: ComponentFixture<ShopButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShopButtonComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ShopButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
