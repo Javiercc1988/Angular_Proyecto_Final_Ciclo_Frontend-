1c3fd6ab720ffb830369be26253d15142a435de7
Javier Callejón <javicallejon88@gmail.com>
2022-05-24 17:06:57 +0200

    Rutas securizadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,24 +10,25 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { Page404Component } from './pages/page404/page404.component';
 import { ProductsComponent } from './pages/products/products.component';
+import { AuthGuard } from './servicios/auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'products', component: ProductsComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard]  },
+  { path: 'admin/productForm', component: ProductFormComponent, canActivate: [AuthGuard]  },
+  { path: 'admin/categoryForm', component: CategoryFormComponent, canActivate: [AuthGuard]  },
+  { path: 'admin/userForm', component: UserFormComponent, canActivate: [AuthGuard]  },
+  { path: 'login', component: LoginPageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: Page404Component },
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      { path: 'home', component: HomeComponent },
-      { path: 'about-us', component: AboutUsComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: 'products', component: ProductsComponent },
-      { path: 'admin', component: AdminComponent },
-      { path: 'admin/productForm', component: ProductFormComponent },
-      { path: 'admin/categoryForm', component: CategoryFormComponent },
-      { path: 'admin/userForm', component: UserFormComponent },
-      { path: 'login', component: LoginPageComponent },
-      { path: '', redirectTo: 'home', pathMatch: 'full' },
-      { path: '**', component: Page404Component },
-    ]),
+    RouterModule.forRoot(routes),
   ],
   exports: [RouterModule],
 })
--- src/app/servicios/auth/auth.service.ts
+++ src/app/servicios/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { IUserLogin } from 'src/app/interfaces/IUserLogin.interface';
 import { environment } from 'src/environments/environment';
+import { SessionStorageService } from '../session-storage/session-storage.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,23 @@ export class AuthService {
   apiUrl: string = environment.apiAuth; //'api/auth'
   url: string;
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private sessionStorage: SessionStorageService
+    ) {
     this.url = `${this.baseUrl}/${this.apiUrl}`;
   }
 
   getLogin(userLogin: IUserLogin) {
+
+    this.sessionStorage.set('user', userLogin.correo)
     return this.http.post<any>(`${this.url}/login`, userLogin);
+  }
+
+  isLogged(){
+    const user = this.sessionStorage.get('user')
+
+    return (!user) ? false : true
   }
 
 }
